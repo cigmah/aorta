ab076a591336a1f9f85bf429187a419e159a4b74
wjmn <Jamin.Wu07@gmail.com>
2020-10-03 22:40:40 +1000

    Updated elmapp.config.js. for production build.

--- elmapp.config.js
+++ elmapp.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     homepage: "https://aorta.cigmah.org",
     configureWebpack: (config, env) => {
-        if (env.NODE_ENV == "production") {
+        if (env.NODE_ENV != "development") {
             config.optimization.minimizer[0].options.uglifyOptions.compress.passes = 1;
             config.optimization.minimizer[0].options.uglifyOptions.compress.unsafe_comps = false;
             config.optimization.minimizer[0].options.uglifyOptions.compress.unsafe = false;
