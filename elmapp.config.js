8b2a248e1342de3538f3ce2c27b49520e5fa292f
wjmn <Jamin.Wu07@gmail.com>
2020-10-03 18:47:52 +1000

    elmapp.config fix for development env

--- elmapp.config.js
+++ elmapp.config.js
@@ -1,9 +1,11 @@
 module.exports = {
     homepage: "https://aorta.cigmah.org",
     configureWebpack: (config, env) => {
-        config.optimization.minimizer[0].options.uglifyOptions.compress.passes = 1;
-        config.optimization.minimizer[0].options.uglifyOptions.compress.unsafe_comps = false;
-        config.optimization.minimizer[0].options.uglifyOptions.compress.unsafe = false;
+        if (env.NODE_ENV == "production") {
+            config.optimization.minimizer[0].options.uglifyOptions.compress.passes = 1;
+            config.optimization.minimizer[0].options.uglifyOptions.compress.unsafe_comps = false;
+            config.optimization.minimizer[0].options.uglifyOptions.compress.unsafe = false;
+        }
         // Manipulate the config object and return it.
         return config;
     }
